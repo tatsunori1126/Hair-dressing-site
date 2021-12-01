dfed6c0276e9effd9eada21ea4e6a9960f050983
tatsunori <iwai.tatsunori1126@gmail.com>
2021-12-01 17:09:42 +0900

    2021.12.01_アニメーション完成（確認前）

--- js/script.js
+++ js/script.js
@@ -8,3 +8,52 @@ $(function () {
         $(".p-header__nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
     });
 });
+
+function GlowAnimeControl() {
+    $('.p-main__top-visual-title').each(function() {
+
+        var elemPos = $(this).offset().top - 50;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        if (scroll >= elemPos - windowHeight) {
+            $(this).addClass("glow");
+
+        } else {
+            $(this).removeClass("glow");
+        }
+    });
+}
+$(window).on('load', function() {
+    var element = $(".p-main__top-visual-title");
+    element.each(function() {
+        var text = $(this).text();
+        var textbox = "";
+        text.split('').forEach(function (t, i) {
+            if (t !== " ") {
+                if (i < 10) {
+                    textbox += '<span style="animation-delay:.' + i + 's;">' + t + '</span>';
+                } else {
+                    var n = i / 10;
+                    textbox += '<span style="animation-delay:.' + n + 's;">' + t + '</span>';
+                }
+            } else {
+                textbox += t;
+            }
+        });
+        $(this).html(textbox);
+    });
+    GlowAnimeControl();
+});
+
+$(window).scroll(function () {
+    $('.blur-trigger').each(function() {
+        var elemPos = $(this).offset().top-50;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        if (scroll >= elemPos - windowHeight) {
+            $(this).addClass('blur');
+        } else {
+            $(this).removeClass('blur');
+        }
+    });
+});
